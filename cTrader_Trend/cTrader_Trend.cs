71bcdce0a90e2439e94644d1f62ceea0d455c2a9
Julian Einzinger <julian5einzinger@gmail.com>
2024-11-14 19:08:10 +0100

    Added very basic moving avg. behaviour

--- cTrader_Trend/cTrader_Trend.cs
+++ cTrader_Trend/cTrader_Trend.cs
@@ -1,33 +1,63 @@
-using System;
 using cAlgo.API;
-using cAlgo.API.Collections;
 using cAlgo.API.Indicators;
 using cAlgo.API.Internals;
 
 namespace cAlgo.Robots
 {
-    [Robot(AccessRights = AccessRights.None, AddIndicators = true)]
-    public class cTrader_Trend : Robot
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class MovingAverageCrossoverBot : Robot
     {
-        [Parameter(DefaultValue = "Hello world!")]
-        public string Message { get; set; }
+        // Parameter für die beweglichen Durchschnitte
+        [Parameter("Short Period", DefaultValue = 9)]
+        public int ShortPeriod { get; set; }
+
+        [Parameter("Long Period", DefaultValue = 21)]
+        public int LongPeriod { get; set; }
+
+        [Parameter("Volume", DefaultValue = 10000)]
+        public int Volume { get; set; }
+
+        private MovingAverage _shortMa;
+        private MovingAverage _longMa;
+        private Position _position;
 
         protected override void OnStart()
         {
-            // To learn more about cTrader Automate visit our Help Center:
-            // https://help.ctrader.com/ctrader-automate
-
-            Print(Message);
+            // Initialisierung der Indikatoren
+            _shortMa = Indicators.MovingAverage(Bars.ClosePrices, ShortPeriod, MovingAverageType.Exponential);
+            _longMa = Indicators.MovingAverage(Bars.ClosePrices, LongPeriod, MovingAverageType.Exponential);
         }
 
-        protected override void OnTick()
+        protected override void OnBar()
         {
-            // Handle price updates here
+            // Überprüfen, ob eine offene Position besteht
+            _position = Positions.Find("MovingAverageCrossoverBot", SymbolName);
+
+            // Bedingungen für Kaufsignal
+            if (_shortMa.Result.LastValue > _longMa.Result.LastValue)
+            {
+                if (_position == null)
+                {
+                    ExecuteMarketOrder(TradeType.Buy, SymbolName, Volume, "MovingAverageCrossoverBot");
+                }
+            }
+            // Bedingungen für Verkaufssignal
+            else if (_shortMa.Result.LastValue < _longMa.Result.LastValue)
+            {
+                if (_position != null)
+                {
+                    ClosePosition(_position);
+                }
+            }
         }
 
         protected override void OnStop()
         {
-            // Handle cBot stop here
+            // Alle Positionen schließen, wenn der Bot angehalten wird
+            foreach (var position in Positions.FindAll("MovingAverageCrossoverBot", SymbolName))
+            {
+                ClosePosition(position);
+            }
         }
     }
 }
