c77820f01ac0b9095a71b551bd78e1c9500b540d
Julian Einzinger <julian5einzinger@gmail.com>
2024-11-15 20:24:48 +0100

    Added tools class

---
+++ cTrader_Trend/Tools.cs
@@ -0,0 +1,19 @@
+﻿using cAlgo.API;
+
+namespace TestBot {
+    internal class Tools : Robot {
+
+        public static void Clean(Robot instance) {
+            for (int i = 0; i < 10; i++) {
+                instance.Print();
+            }
+        }
+
+        public static double CalculateRelativePriceInPips(Robot instance, double targetPrice) {
+            var pipValue = instance.Symbol.PipValue * instance.Symbol.TickSize;
+            var pips = Math.Abs(targetPrice / pipValue);
+
+            return pips;
+        }
+    }
+}
